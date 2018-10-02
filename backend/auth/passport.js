bb72229a63338060a8388762cc9a98420b3647c9
Jeff Tanner <jeff00seattle@gmail.com>
2018-10-02 05:11:42 -0700

    [Step-1] Auth

--- backend/auth/passport.js
+++ backend/auth/passport.js
@@ -11,11 +11,11 @@ done(null, user);
 
 passport.use(
 	new GoogleStrategy(
-		{
-			clientID: "631644900154-b2rf0ft11ft3sqjmprg2obc0pmpgr0u7.apps.googleusercontent.com",
-			clientSecret: "go1-JjTSWBWVNMw1jzlOE5O1",
-			callbackURL: "http://localhost:4500/auth/google/callback"
-		},
+        {
+            clientID: "GOOGLE_CLIENT_ID",
+            clientSecret: "GOOGLE_CLIENT_SECRET",
+            callbackURL: "http://localhost:4500/auth/google/callback"
+        },
 		function(accessToken, refreshToken, profile, done) {
 			var userData = {
 				email: profile.emails[0].value,
